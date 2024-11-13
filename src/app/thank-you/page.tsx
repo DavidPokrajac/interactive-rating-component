a9145c5b9e0f39fe84fcef1436b27b205f9a43d5
David <d0pokrajac@gmail.com>
2024-11-13 12:19:02 +0100

    Added Suspense boundary

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
@@ -6,23 +7,25 @@ export default function ThankYou() {
     const searchParams = useSearchParams();
     const search = searchParams.get("rating");
     return (
-        <article className="[ wrapper flow ] [ padding-l text-center ]">
-            <figure className="[ image-center bg-transparent ]">
-                <Image
-                    src="/images/illustration-thank-you.svg"
-                    width={162}
-                    height={108}
-                    alt=""
-                />
-            </figure>
-            <span className="[ inline-block text-orange bg-light padding-sm border-radius-md ]">
-                You selected out {search} of 5
-            </span>
-            <p className="[ fs-l fw-bold text-white ]">Thank you!</p>
-            <p>
-                We appreciate you taking the time to give a rating. If you ever
-                need more support, don’t hesitate to get in touch!
-            </p>
-        </article>
+        <Suspense>
+            <article className="[ wrapper flow ] [ padding-l text-center ]">
+                <figure className="[ image-center bg-transparent ]">
+                    <Image
+                        src="/images/illustration-thank-you.svg"
+                        width={162}
+                        height={108}
+                        alt=""
+                    />
+                </figure>
+                <span className="[ inline-block text-orange bg-light padding-sm border-radius-md ]">
+                    You selected out {search} of 5
+                </span>
+                <p className="[ fs-l fw-bold text-white ]">Thank you!</p>
+                <p>
+                    We appreciate you taking the time to give a rating. If you
+                    ever need more support, don’t hesitate to get in touch!
+                </p>
+            </article>
+        </Suspense>
     );
 }
